c98cb7a912b9287975b8218374a091940622a3bc
dev-ethanjohn <dev.ejohnpaguntalan@gmail.com>
2023-10-30 10:01:33 -0700

    Add Initial Navbar Component

---
+++ scrimba-react/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const navbar = (
+  <nav>
+    <h1>Ethan React App</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+// ReactDOM.render(navbar, document.getElementById('root'));
+// ReactDOM.createRoot(document.getElementById('root').render(navbar));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(navbar);
