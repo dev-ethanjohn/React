9827a68d3237094601c0f96039e4f01a7c546a78
dev-ethanjohn <dev.ejohnpaguntalan@gmail.com>
2023-10-30 20:36:08 -0700

    Create custom React component for practice

--- scrimba-react/src/index.js
+++ scrimba-react/src/index.js
@@ -1,18 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const navbar = (
-  <nav>
-    <h1>Ethan React App</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-);
+// function TemporaryName() {
+//   return (
+//     <div>
+//       <img src="/images/react.png" width="40px" alt="react logo" />
+//       <h1>Fun facts about React</h1>
+//       <ul>
+//         <li>Was released in 2013</li>
+//         <li>Was originally created by Jordan Walke</li>
+//         <li>Has well over 100k stars on Github</li>
+//         <li>Is maintained by Facebook</li>
+//         <li>Powers thousands of enterprise apps, including mobile apps</li>
+//       </ul>
+//     </div>
+//   );
+// }
 
-// ReactDOM.render(navbar, document.getElementById('root'));
-// ReactDOM.createRoot(document.getElementById('root').render(navbar));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<TemporaryName />);
+
+//------------------------------------
+
+/**
+Challenge: 
+
+Part 1: Create a page of your own using a custom Page component
+
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
+
+Render your list to the page
+
+ */
+
+function Hero() {
+  return (
+    <div>
+      <h1>Reasons why React is so a must have JS library</h1>
+      <ol>
+        <li>React is versatile and easy to use.</li>
+        <li>In demand and most used JS library.</li>
+        <li>Building programs with React is declarative and composable</li>
+      </ol>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(navbar);
+
+root.render(<Hero />);
