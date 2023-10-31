db6550be13c6f0e0e1e781c7b770788ee91e516c
dev-ethanjohn <dev.ejohnpaguntalan@gmail.com>
2023-10-30 23:12:40 -0700

    Add lesson on React Parent-Child component relationship

--- scrimba-react/src/index.js
+++ scrimba-react/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 // function TemporaryName() {
 //   return (
@@ -37,29 +37,54 @@ PART 2:
 - Add a `header` element with a nested `nav` element. Inside the `nav`, include a `img` element with the image of the React logo and make sure to set the width to something more manageable so it doesn't take up the whole screen. 
 - Add an `h1` w/ some text describing the page.
 - Add a `footer` after the list that says: "© 20xx <last name here> development. All rights reserved."
+
+
+Move the `header` element from Hero into 
+its own component called "Header"
  */
 
-function Hero() {
+// CHILDREN
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="/images/react.png" alt="react logo" width="200px" />
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="/images/react.png" alt="react logo" width="200px" />
-        </nav>
-      </header>
-      <h1>Reasons why React is so a must have JS library</h1>
+      <h1>Reasons why React is a must-have JS library</h1>
       <ol>
         <li>React is versatile and easy to use.</li>
-        <li>In demand and most used JS library.</li>
-        <li>Building programs with React is declarative and composable</li>
+        <li>In demand and the most used JS library.</li>
+        <li>Building programs with React is declarative and composable.</li>
       </ol>
-      <footer>
-        <small>© 2021 Paguntalan development. All rights reserved.</small>
-      </footer>
     </div>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Footer() {
+  return (
+    <footer>
+      <small>© 2021 Paguntalan development. All rights reserved.</small>
+    </footer>
+  );
+}
 
-root.render(<Hero />);
+// SKELETON (PARENT)
+function Hero() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<Hero />, root);
